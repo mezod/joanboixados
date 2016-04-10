4982ecd6a26abb8b5a2683b237da7d0ebde3302f
mezod <mezood@gmail.com>
2016-04-10 02:24:50 +0200

    server

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-/* const serve = require('koa-static');
+const serve = require('koa-static');
 const send = require('koa-send');
 const koa = require('koa');
 const app = koa();
 const port = 8080;
 
 app.use(serve(__dirname + '/dist'));
-app.use(serve(__dirname + '/api/galleries'));
 
 app.use(function* index() {
   yield send(this, './dist/index.html');
 });
 
-app.listen(port); */
+app.listen(port);
