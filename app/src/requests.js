6707abce21f0c5380667b26a6e94d21a119cb16a
Joan Boixadós <mezood@gmail.com>
2022-08-09 23:55:04 +0200

    Update requests.js

--- app/src/requests.js
+++ app/src/requests.js
@@ -6,7 +6,7 @@ export function fetchGalleries() {
     'Content-Type': 'application/json',
   };
 
-  return fetch(`http://${config.apiUrl}/menu`, {
+  return fetch(`https://${config.apiUrl}/menu`, {
     method: 'GET',
     headers: headers,
   }).then(res => {
@@ -19,7 +19,7 @@ export function fetchImages(gallery) {
     'Content-Type': 'application/json',
   };
 
-  return fetch(`http://${config.apiUrl}/` + gallery, {
+  return fetch(`https://${config.apiUrl}/` + gallery, {
     method: 'GET',
     headers: headers,
   }).then(res => {
